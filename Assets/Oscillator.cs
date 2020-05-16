51e28406f25c1b018bc14fa14b0c0f1414d3d1e6
Zhenghao Li <lizhenghao99@outlook.com>
2020-05-15 20:02:29 -0700

    demo complete

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = Vector3.up * 5;
+    [SerializeField] [Range(0, 1)] float movementFactor;
+    [SerializeField] float frequency = 0.3f;
+
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float phase = 2 * Mathf.PI * frequency * Time.time;
+        movementFactor = Mathf.Sin(phase);
+
+        transform.position = startingPos + movementVector * movementFactor;
+    }
+}
--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,15 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] float mainThrust = 10f;
+    [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rcsThrust = 50f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
 
     Rigidbody rigidBody;
     AudioSource audioSource;
 
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
+    bool collisonDisabled = false;
     
     // Start is called before the first frame update
     void Start()
@@ -22,36 +36,102 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+        if (Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();
+        }
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisonDisabled = !collisonDisabled;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive || collisonDisabled) { return; }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
+                // do nothing
+                break;
+            case "Finish":
+                state = State.Transcending;
+                audioSource.Stop();
+                audioSource.volume = 0.3f;
+                audioSource.PlayOneShot(success);
+                mainEngineParticles.Stop();
+                successParticles.Play();
+                Invoke("LoadNextLevel", 1f);
                 break;
             default:
-                print("DEAD");
+                state = State.Dying;
+                audioSource.Stop();
+                audioSource.volume = 0.3f;
+                audioSource.PlayOneShot(death);
+                deathParticles.Play();
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        int nextLevel = (currentLevel + 1) % 
+            SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            // movement
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            // audio
             if (!audioSource.isPlaying)
             {
+                audioSource.clip = mainEngine;
                 audioSource.Play();
+            }
+            // particle
+            mainEngineParticles.Play();
+
+            if (audioSource.volume < 0.5f)
+            {
+                audioSource.volume += 0.05f;
             }
         }
         else
         {
-            audioSource.Stop();
+            if (audioSource.volume == 0)
+            {
+                audioSource.Stop();
+            }
+            else
+            {
+                audioSource.volume -= 0.05f;
+            }
+            mainEngineParticles.Stop();
         }
     }
 
